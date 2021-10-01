9c779c2903cca146c53e58cf5d6b60ee429e8724
Minhdang04 <80468760+Minhdang04@users.noreply.github.com>
2021-10-01 23:03:01 +0700

    Add files via upload

---
+++ signin.js
@@ -0,0 +1,2 @@
+//lam sao de tu sign up sang sign in hoac nguoc lai ._.
+//kieu nhu la sign up xong chuyen sang trang sign in hoac chua co tai khoan thi chuyen sang signup ay ._.
---
+++ signup.js
@@ -0,0 +1,3 @@
+const username = document.getElementById("username").value
+const password = document.getElementById("password").value
+const repassword = document.getElementById("repassword").value
